66f953b9f898890d11ecb58c50688a10cf37d06d
molopezCU <52024285+molopezCU@users.noreply.github.com>
2022-05-15 20:04:48 -0700

    ShowImages properly resizes and has bg
    finally

--- Arsenal/navigation/screens/ShowImages.js
+++ Arsenal/navigation/screens/ShowImages.js
@@ -11,7 +11,10 @@ const ShowImages = props => {
 
     return(
         <View>
-            <Image style={{ height: deviceHeight, width: deviceWidth, position: 'absolute', top:0, left:0 }} source={props.route.params.url} />
+      <Image style={{ height: deviceHeight, width: deviceWidth, position: 'absolute', top:0, left:0 }} source={{ uri: 'https://storage.googleapis.com/fabmaster/media/images/upr_key_art_7Zz.width-10000.jpg' }} />
+        <View>
+            <Image style={{ height: deviceHeight / 1.01, width: deviceWidth / 1.01, position: 'absolute', resizeMode: 'contain', top: -60, left:2.5 }} source={props.route.params.url} />
+        </View>
         </View>
     )
 }
